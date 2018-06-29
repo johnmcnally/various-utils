fd4dbb33312244e828e0d09f2e3f58e28dfd10b1
John McNally <jmcnally@acm.org>
2018-06-29 10:56:57 -0400

    Create device_constants.py
    Constants for get-logfile-name.py

---
+++ device_constants.py
@@ -0,0 +1,14 @@
+# Syslog root directory
+SYSLOG_ROOT = '/usr/local/groundwork/common/var/log/syslog-ng'
+
+# Dictionary of currently installed devices
+DEVICES = [
+    {'name': 'buckwheat', 'model': 'U7PG2', 'mac': 'fcecda34e2f5', 'version': '3.9.40.9098'},
+    {'name': 'chia', 'model': 'U7P', 'mac': '44d9e7903567', 'version': '3.9.40.9098'},
+    {'name': 'corn', 'model': 'U7PG2', 'mac': 'fcecda371f1c', 'version': '3.9.40.9098'},
+    {'name': 'fonio', 'model': 'U7P', 'mac': '44d9e7903452', 'version': '3.9.40.9098'},
+    {'name': 'milo', 'model': 'U7PG2', 'mac': '802aa81664f1', 'version': '3.9.40.9098'},
+    {'name': 'sorghum', 'model': 'U7P', 'mac': '44d9e790350e', 'version': '3.9.40.9098'},
+    {'name': 'teff', 'model': 'U7PG2', 'mac': '788a208667f5', 'version': '3.9.40.9098'},
+    {'name': 'wheat', 'model': 'U7PG2', 'mac': '802aa8166684', 'version': '3.9.40.9098'}
+]
