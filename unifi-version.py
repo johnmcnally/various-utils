d013f7519b08f929aec32d45184edadfa6648f24
John McNally <jmcnally@acm.org>
2023-09-14 13:14:54 -0400

    Update unifi-version.py

--- unifi-version.py
+++ unifi-version.py
@@ -1,44 +1,46 @@
-#!/bin/python
-# Name: unifi-version.py
+#!/usr/bin/python3
+# Name: unifi-version
 # Description: Display Ubiquiti UniFi Controller installed version and bundled device firmware
-# Requires: Python 2.6 or later
+# Requires: Python 3.6 or later; UniFi Controller 7 or later
 # Author: John McNally
-# Version: 1.00
-# Release date: 12/8/2016
+# Version: 1.10
+# Release date: 3/15/23
 
 def usage():
-    print "usage: unifi-version.py [-h] [-a]\n \
+    print ("usage: unifi-version [-h] [-a]\n \
     -a, --all        show all bundled firmware\n \
-    -h, --help       show this help message and exit"
+    -h, --help       show this help message and exit")
 
 # MAIN()
 import sys, json
 
 controller_version = open("/opt/UniFi/webapps/ROOT/app-unifi/.version").read()
+controller_version_short = controller_version[:controller_version.find(".",controller_version.rindex("."))]
 bundles_path = "/opt/UniFi/dl/firmware/bundles.json"
 bundles = json.load(open(bundles_path))
-installed_devices = ["UniFi AP-Pro", \
-                     "UniFi AP-AC-Pro", \
-                     "UniFi AP-AC-Pro Gen2"]
+firmware_path = "/opt/UniFi/data/firmware.json"
+firmware = json.load(open(firmware_path))
+installed_devices = ["UniFi AP-AC-Pro", \
+                     "UniFi Flex HD"]
 
 # Installed devices only
 if len(sys.argv) == 1:
-    print "UniFi Controller {0}".format(controller_version[:controller_version.find("-")])
+    print ("UniFi Controller {0}".format(controller_version_short))
     for device in installed_devices:
         for bundle in bundles:
             try:
                 if bundles[bundle]["display"] == device:
-                    print "{0} {1}".format(bundles[bundle]["display"], bundles[bundle]["version"])
+                    print ("{0} {1}".format(bundles[bundle]["display"], firmware[controller_version_short]["release"][bundle]["version"]))
             except KeyError:
                 continue # if version is missing
 
 # All bundles with a "version" attribute. Skip other bundles
 elif len(sys.argv) == 2:
     if sys.argv[1] == "--all" or sys.argv[1] == "-a":
-        print "UniFi Controller {0}".format(controller_version[:controller_version.find("-")])
+        print ("UniFi Controller {0}".format(controller_version_short))
         for bundle in bundles:
             try:
-                print "{0} {1}".format(bundles[bundle]["display"], bundles[bundle]["version"])
+                print ("{0} {1}".format(bundles[bundle]["display"], firmware[controller_version_short]["release"][bundle]["version"]))
             except KeyError:
                 continue # if version is missing
     elif sys.argv[1] == "--help" or sys.argv[1] == "-h":
@@ -48,7 +50,7 @@ elif len(sys.argv) == 2:
         usage()
         sys.exit(1)
 
-# More than two arguments entered
+# More than one argument entered
 else:
     usage()
     sys.exit(1)
