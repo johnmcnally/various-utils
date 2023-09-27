83801b6756f31d0db07ee4b24e6e235862bda85c
John McNally <jmcnally@acm.org>
2023-09-26 22:14:38 -0400

    Update unifi-version.py

--- unifi-version.py
+++ unifi-version.py
@@ -3,8 +3,8 @@
 # Description: Display Ubiquiti UniFi Controller installed version and bundled device firmware
 # Requires: Python 3.6 or later; UniFi Controller 7 or later
 # Author: John McNally
-# Version: 1.10
-# Release date: 3/15/23
+# Version: 1.11
+# Release date: 9/26/23
 
 def usage():
     print ("usage: unifi-version [-h] [-a]\n \
@@ -25,22 +25,22 @@ installed_devices = ["UniFi AP-AC-Pro", \
 
 # Installed devices only
 if len(sys.argv) == 1:
-    print ("UniFi Controller {0}".format(controller_version_short))
+    print (f"UniFi Controller {controller_version_short}")
     for device in installed_devices:
         for bundle in bundles:
             try:
                 if bundles[bundle]["display"] == device:
-                    print ("{0} {1}".format(bundles[bundle]["display"], firmware[controller_version_short]["release"][bundle]["version"]))
+                    print (bundles[bundle]["display"], firmware[controller_version_short]["release"][bundle]["version"])
             except KeyError:
                 continue # if version is missing
 
 # All bundles with a "version" attribute. Skip other bundles
 elif len(sys.argv) == 2:
     if sys.argv[1] == "--all" or sys.argv[1] == "-a":
-        print ("UniFi Controller {0}".format(controller_version_short))
+        print (f"UniFi Controller {controller_version_short}")
         for bundle in bundles:
             try:
-                print ("{0} {1}".format(bundles[bundle]["display"], firmware[controller_version_short]["release"][bundle]["version"]))
+                print (bundles[bundle]["display"], firmware[controller_version_short]["release"][bundle]["version"])
             except KeyError:
                 continue # if version is missing
     elif sys.argv[1] == "--help" or sys.argv[1] == "-h":
